8af5aca146cd7335375f081f3590c422ee2dd1fd
zy <181332929@qq.com>
2021-03-09 09:21:23 +0800

    annotation

--- src/java8_Stream/Demo1_ForEach.java
+++ src/java8_Stream/Demo1_ForEach.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Demo1_ForEach {
+    //asdasd
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("cv", "abd", "aba", "efg", "abcd", "jkl", "jkl");
         strings.stream().forEach(str -> System.out.println(str));
