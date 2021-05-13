0f6c62b9d6f19281c3aadf71144c2cd689f8754b
zhouyu0314 <181332969@qq.com>
2021-05-13 17:55:32 +0800

    三种代理模式

--- src/jvm/allocation/Demo01.java
+++ src/jvm/allocation/Demo01.java
@@ -8,12 +8,14 @@ public class Demo01 {
 
     public static void main(String[] args) {
         //testAllocation();
-        testPretenureSizeThreshold();
+        //testPretenureSizeThreshold();
+        testTenuringThreshold();
 
 
     }
 
     /**
+     * 新生代Minor GC
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
      */
     public static void testAllocation() {
@@ -36,6 +38,7 @@ public class Demo01 {
     }
 
     /**
+     * 大对象直接进入老年代
      * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
      * -XX:PretenureSizeThreshold=3145728
      */
@@ -53,6 +56,41 @@ public class Demo01 {
          Metaspace       used 3057K, capacity 4556K, committed 4864K, reserved 1056768K
           class space    used 324K, capacity 392K, committed 512K, reserved 1048576K
         Disconnected from the target VM, address: '127.0.0.1:1952', transport: 'socket'
+         */
+    }
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:Survivor-
+     * Ratio=8 -XX:MaxTenuringThreshold=1
+     * -XX:+PrintTenuringDistribution
+     */
+    @SuppressWarnings("unused")
+    public static void testTenuringThreshold() {
+        byte[] allocation1, allocation2, allocation3;
+        allocation1 = new byte[_1MB / 4]; // 什么时候进入老年代决定于XX:MaxTenuring-Threshold设置
+        allocation2 = new byte[4 * _1MB];
+        allocation3 = new byte[4 * _1MB];
+        allocation3 = null;
+        allocation3 = new byte[4 * _1MB];
+
+
+        /*
+        [GC (Allocation Failure) [ParNew
+        Desired survivor size 524288 bytes, new threshold 1 (max 1)
+        - age   1:     878632 bytes,     878632 total
+        : 6341K->878K(9216K), 0.0048594 secs] 6341K->4977K(19456K), 0.0054887 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+        [GC (Allocation Failure) [ParNew
+        Desired survivor size 524288 bytes, new threshold 1 (max 1)
+        - age   1:        800 bytes,        800 total
+        : 5059K->482K(9216K), 0.0019171 secs] 9157K->5445K(19456K), 0.0019404 secs] [Times: user=0.05 sys=0.00, real=0.00 secs]
+        Heap
+         par new generation   total 9216K, used 4716K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
+          eden space 8192K,  51% used [0x00000000fec00000, 0x00000000ff022760, 0x00000000ff400000)
+          from space 1024K,  47% used [0x00000000ff400000, 0x00000000ff478ba0, 0x00000000ff500000)
+          to   space 1024K,   0% used [0x00000000ff500000, 0x00000000ff500000, 0x00000000ff600000)
+         concurrent mark-sweep generation total 10240K, used 4962K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
+         Metaspace       used 3208K, capacity 4496K, committed 4864K, reserved 1056768K
+          class space    used 347K, capacity 388K, committed 512K, reserved 1048576K
          */
     }
 }
