d7e49d5458b5044e6fcbbf6764764e949308fc47
zy <181332969@qq.com>
2021-05-17 22:07:36 +0800

    jvm

---
+++ src/jvm/visualTool/JHSDB_TestCase.java
@@ -0,0 +1,25 @@
+package jvm.visualTool;
+
+/**
+ * staticObj、instanceObj、localObj存放在哪里？
+ * -Xmx10m -XX:+UseSerialGC -XX:-UseCompressedOops
+ */
+public class JHSDB_TestCase {
+    static class Test {
+        static ObjectHolder staticObj = new ObjectHolder();
+        ObjectHolder instanceObj = new ObjectHolder();
+
+        void foo() {
+            ObjectHolder localObj = new ObjectHolder();
+            System.out.println("done"); // 这里设一个断点
+        }
+    }
+
+    private static class ObjectHolder {
+    }
+
+    public static void main(String[] args) {
+        Test test = new JHSDB_TestCase.Test();
+        test.foo();
+    }
+}
